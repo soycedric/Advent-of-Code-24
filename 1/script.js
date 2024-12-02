0919dcd4c5625e5d58977ab56ab8311469515bb5
= <=>
2024-12-02 14:11:33 -0600

    second commit

--- 1/script.js
+++ 1/script.js
@@ -28,15 +28,33 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   leftArray.sort((a, b) => b - a).reverse();
   rightArray.sort((a, b) => b - a).reverse();
 
-  console.log("Números de la izquierda:", leftArray);
-  console.log("Números de la derecha:", rightArray);
+ // console.log("Números de la izquierda:", leftArray);
+ // console.log("Números de la derecha:", rightArray);
 
-  const combinedSum = leftArray.reduce((acc, num, index) => {
-    const difference = num - rightArray[index]; // Suma el elemento de array1 y array2 en el índice actual
-    const absoluteDifference = difference < 0 ? -difference : difference;
+  var combinedSum = leftArray.reduce((acc, num, index) => {
+    var difference = num - rightArray[index]; // Suma el elemento de array1 y array2 en el índice actual
+    var absoluteDifference = difference < 0 ? -difference : difference;
     return acc + absoluteDifference;
   }, 0);
-  console.log("Suma combinada de las diferencias:", combinedSum);
+ // console.log("Suma combinada de las diferencias:", combinedSum);
+
+  var counts = leftArray.reduce((acc, num) => {
+    var count = rightArray.filter(item => item === num).length; // Contar ocurrencias de num en array2
+    if (count > 0) {
+      acc[num] = count; // Solo almacenar si hay ocurrencias
+    };
+    return acc;
+  }, {});
+  const keys = Object.keys(counts);
+  const values = Object.values(counts);
+  let finalSum = 0;
+  for (let key of keys){
+   // console.log(key);
+   // console.log(counts[key]);
+    finalSum += key*counts[key];
+  } 
+  console.log("Cuentas de cada número:", finalSum);
+  
 });
 
 
